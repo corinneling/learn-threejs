e566dbf19b9f728eaddcec03aedb6807b72aa840
corinne <corinne@heysparkbox.com>
2019-02-02 16:09:48 -0500

    feat: refactor index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,27 @@
-var THREE = require('three');
+const THREE = require('three');
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-var renderer = new THREE.WebGLRenderer({ alpha: true });
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor( 0xffffff, 0);
-
 document.body.appendChild(renderer.domElement);
 
-// the cube
-var geometry = new THREE.BoxGeometry(2, 2, 2);
-var material = new THREE.MeshBasicMaterial({
+const geometry = new THREE.BoxGeometry(2, 2, 2);
+const material = new THREE.MeshBasicMaterial({
   color: 0x00bbff
 });
-var cube = new THREE.Mesh(geometry, material);
-// add the cube
+const cube = new THREE.Mesh(geometry, material);
+
 scene.add(cube);
-// set the camera
 camera.position.z = 5;
 
-function animate() {
+function renderScene() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
 }
 
-animate();
+renderScene();
